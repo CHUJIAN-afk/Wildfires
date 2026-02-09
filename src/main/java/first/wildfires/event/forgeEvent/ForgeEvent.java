9ec9ed6bcca625fbe0dd39bb452f694b0b6356d9
GIG <liuhong.bing@qq.com>
2026-02-10 00:28:29 +0800

    4

--- src/main/java/first/wildfires/event/forgeEvent/ForgeEvent.java
+++ src/main/java/first/wildfires/event/forgeEvent/ForgeEvent.java
@@ -6,7 +6,9 @@ import first.wildfires.utils.WildfiresUtil;
 import net.dries007.tfc.common.capabilities.food.FoodCapability;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
@@ -14,14 +16,38 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.ItemAttributeModifierEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Wildfires.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeEvent {
+
+	@SubscribeEvent
+	public static void hurt(LivingHurtEvent event) {
+		if (event.getSource().getEntity() instanceof LivingEntity attacker) {
+			ItemStack item = attacker.getMainHandItem();
+			if (item.is(Tags.Items.TOOLS)) {
+				LivingEntity target = event.getEntity();
+				AABB aabb = target.getBoundingBox();
+				Vec3 center = attacker.getBoundingBox().getCenter();
+				double dx = Math.max(aabb.minX - center.x, Math.max(0, center.x - aabb.maxX));
+				double dy = Math.max(aabb.minY - center.y, Math.max(0, center.y - aabb.maxY));
+				double dz = Math.max(aabb.minZ - center.z, Math.max(0, center.z - aabb.maxZ));
+				float distance = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+				if (distance <= 4) {
+					event.setAmount(event.getAmount() * (0.5f + (distance * 0.125f)));
+				}
+			}
+		}
+	}
 
 	@SubscribeEvent
 	public static void onBreakSpeed(PlayerEvent.BreakSpeed event) {

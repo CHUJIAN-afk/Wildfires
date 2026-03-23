6d674d104c0a37b4286185e99c2a2fc9aa3cf215
GIG <liuhong.bing@qq.com>
2026-03-23 13:10:57 +0800

    小改

--- src/main/java/first/wildfires/api/customEvent/StructureStageModifyEvent.java
+++ src/main/java/first/wildfires/api/customEvent/StructureStageModifyEvent.java
@@ -4,12 +4,17 @@ import first.wildfires.utils.WildfiresUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.Event;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StructureStageModifyEvent extends Event {
 
     public void addStage(String stage, List<ResourceLocation> structureList) {
         WildfiresUtil.StructureStageMap.put(stage, structureList);
+    }
+
+    public void addStructure(String stage, ResourceLocation structure) {
+        WildfiresUtil.StructureStageMap.getOrDefault(stage, new ArrayList<>()).add(structure);
     }
 
 }

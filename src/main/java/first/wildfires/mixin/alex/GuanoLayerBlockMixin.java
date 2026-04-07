bffceb953a029961d7ccec13927f39af65fd9bb2
GIG <liuhong.bing@qq.com>
2026-04-07 13:40:58 +0800

    小改

--- src/main/java/first/wildfires/mixin/alex/GuanoLayerBlockMixin.java
+++ src/main/java/first/wildfires/mixin/alex/GuanoLayerBlockMixin.java
@@ -2,22 +2,28 @@ package first.wildfires.mixin.alex;
 
 import com.github.alexmodguy.alexscaves.server.block.GuanoLayerBlock;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = GuanoLayerBlock.class, remap = false)
 public class GuanoLayerBlockMixin {
 
-    @ModifyExpressionValue(
-            method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;of()Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;"
-            )
+    @Inject(
+            method = "getCollisionShape",
+            at = @At("RETURN"),
+            cancellable = true
     )
-    private static BlockBehaviour.Properties injectRandomTicks(BlockBehaviour.Properties original) {
-        return original.noCollission();
+    private void getCollisionShape(BlockState state, BlockGetter level, BlockPos blockPos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir){
+        cir.setReturnValue(Shapes.empty());
     }
 
 }
